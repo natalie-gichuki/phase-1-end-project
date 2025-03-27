d9406ef9751b5327961c2973f37ae1f6efee9a4e
NatalieGichuki <nataliegichuki@gmail.com>
2025-03-27 21:57:16 +0300

    formatted js

--- script.js
+++ script.js
@@ -176,7 +176,7 @@ document.addEventListener("DOMContentLoaded", () => {
         e.preventDefault();
         document.getElementById("updated-modal").style.display = "none";
     })
-    
+
     form.addEventListener("submit", function (e) {
         e.preventDefault();
 
